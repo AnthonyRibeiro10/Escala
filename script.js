7ccd4503fb872bb54566d66b9a6b2314428993b5
AnthonyRibeiro10 <114880568+AnthonyRibeiro10@users.noreply.github.com>
2024-05-16 19:15:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,242 @@
+// JSON com as informações das escalas
+const dados = {
+  "sabados": [
+    {
+      "Data": "01/06/2024",
+      "Bateria": "Anthony",
+      "Baixo": "Abraão",
+      "Violão": "Eduarda",
+      "Guitarra": "Diego (Mic Azul)",
+      "Teclado": "Gabriel Lima",
+      "Saxofone": "Eliseu",
+      "Voz1": "Eduarda (Repertório)",
+      "Voz2": "Kauane",
+      "Voz3": "Nicolas",
+      "Sonoplasta": "Deivid"
+    },
+    {
+      "Data": "08/06/2024",
+      "Bateria": "Anthony/Isabeli",
+      "Baixo": "Felipe Raup",
+      "Violão": "Ismael",
+      "Guitarra": "Eneias (Mic Azul)",
+      "Teclado": "Gabriel Fernandes",
+      "Voz1": "Tainara (Repertório)",
+      "Voz2": "Lidya",
+      "Voz3": "Ismael",
+      "Sonoplasta": "Pedro"
+    },
+    {
+      "Data": "15/06/2024",
+      "Bateria": "Davi",
+      "Baixo": "Abraão",
+      "Violão": "Eduarda",
+      "Guitarra": "Anthony (Mic Azul)",
+      "Teclado": "Gabriel Lima",
+      "Voz1": "Tielen (Repertório)",
+      "Voz2": "Eduarda",
+      "Voz3": "Gabriel Figueiredo",
+      "Sonoplasta": "Deivid/Pedrinho"
+    },
+    {
+      "Data": "22/06/2024",
+      "Bateria": "Davi",
+      "Baixo": "Felipe Raup",
+      "Violão": "Ismael",
+      "Guitarra": "Eneias (Mic Azul)",
+      "Teclado": "Gabriel Fernandes",
+      "Saxofone": "Eliseu",
+      "Voz1": "Tainara (Repertório)",
+      "Voz2": "Kelly",
+      "Voz3": "Kauã",
+      "Sonoplasta": "Pedro"
+    }
+  ],
+ "domingos": [
+     {
+       "Data": "02/06/2024",
+       "Bateria": "Wilson",
+       "Baixo": "Abraão",
+       "Violão": "Felipe Raup",
+       "Guitarra": "Anthony (Mic Azul)",
+       "Teclado": "Diego",
+       "Saxofone": "",
+       "Voz1": "Ana (Repertório)",
+       "Voz2": "Tainara",
+       "Voz3": "Gabriel Figueiredo",
+       "Sonoplasta": "Fábio"
+     },
+     {
+       "Data": "09/06/2024",
+       "Bateria": "Anthony",
+       "Baixo": "Silas",
+       "Violão": "Gabriel Lima (Mic Azul)",
+       "Guitarra": "Eneias",
+       "Teclado": "Gabriel Fernandes",
+       "Saxofone": "Eliseu",
+       "Voz1": "Keila (Repertório)",
+       "Voz2": "Kelly",
+       "Voz3": "Kauã",
+       "Sonoplasta": "Deivid"
+     },
+     {
+       "Data": "16/06/2024",
+       "Bateria": "Jonatas",
+       "Baixo": "Juarez",
+       "Violão": "Eneias",
+       "Guitarra": "Anthony",
+       "Teclado": "Wilson (Mic Azul)",
+       "Saxofone": "",
+       "Voz1": "Daniela (Repertório)",
+       "Voz2": "Lidya",
+       "Voz3": "Nicolas",
+       "Sonoplasta": "Deivid"
+     },
+     {
+       "Data": "23/06/2024",
+       "Bateria": "Wilson",
+       "Baixo": "Eduardo",
+       "Violão": "Gabriel Lima",
+       "Guitarra": "Eneias (Mic Azul)",
+       "Teclado": "Gabriel Fernandes",
+       "Saxofone": "Eliseu",
+       "Voz1": "Gabriel Lima (Repertório)",
+       "Voz2": "Kauane",
+       "Voz3": "Ana",
+       "Sonoplasta": "Pedro"
+     },
+     {
+       "Data": "30/06/2024 - Manhã",
+       "Bateria": "Davi",
+       "Baixo": "Abraão",
+       "Violão": "Felipe Raup",
+       "Guitarra": "Anthony (Mic Azul)",
+       "Teclado": "Nicolas",
+       "Saxofone": "Eliseu",
+       "Voz1": "Tielen (Repertório)",
+       "Voz2": "Eduarda",
+       "Voz3": "Kelly",
+       "Sonoplasta": "Silas"
+     },
+     {
+       "Data": "30/06/2024 - Noite",
+       "Bateria": "Jonatas",
+       "Baixo": "Silas",
+       "Violão": "Gabriel Lima",
+       "Guitarra": "Eneias (Mic Azul)",
+       "Teclado": "Wilson",
+       "Saxofone": "Eliseu",
+       "Voz1": "Keila (Repertório)",
+       "Voz2": "Daniela",
+       "Voz3": "Ana",
+       "Sonoplasta": "Fábio"
+     }
+ ],
+ "quintas": [
+     {
+        "Data": "06/06/2024",
+        "Bateria": "Jonatas",
+        "Baixo": "Silas",
+        "Violão": "Gabriel Lima (Mic Azul)",
+        "Guitarra": "Diego",
+        "Teclado": "Gabriel Fernandes",
+        "Saxofone": "",
+        "Voz1": "Ana (Repertório)",
+        "Voz2": "Eduarda",
+        "Voz3": "Gabriel Lima",
+        "Sonoplasta": "Felipe"
+     },
+     {
+       "Data": "13/06/2024",
+       "Bateria": "Davi",
+       "Baixo": "Eduardo",
+       "Violão": "Ismael",
+       "Guitarra": "Anthony (Mic Azul)",
+       "Teclado": "Nicolas",
+       "Saxofone": "Eliseu",
+       "Voz1": "Keila (Repertório)",
+       "Voz2": "Tielen",
+       "Voz3": "Kauã",
+       "Sonoplasta": "Felipe"
+     },
+     {
+       "Data": "20/06/2024",
+       "Bateria": "Anthony",
+       "Baixo": "Silas",
+       "Violão": "Gabriel Lima",
+       "Guitarra": "Anthony",
+       "Teclado": "Wilson (Mic Azul)",
+       "Saxofone": "",
+       "Voz1": "Daniela (Repertório)",
+       "Voz2": "Kelly",
+       "Voz3": "Nicolas",
+       "Sonoplasta": "Felipe"
+     },
+     {
+       "Data": "27/06/2024",
+       "Bateria": "Anthony",
+       "Baixo": "Eduardo",
+       "Violão": "Gabriel Lima",
+       "Guitarra": "Anthony (Mic Azul)",
+       "Teclado": "Gabriel Fernandes",
+       "Saxofone": "Eliseu",
+       "Voz1": "Gabriel Lima (Repertório)",
+       "Voz2": "Kauane",
+       "Voz3": "Keila",
+       "Sonoplasta": "Felipe"
+     }
+ ],
+    // escalas para outros dias...
+      "todos": [] // Array vazio para incluir todas as escalas
+};
+
+// Função para buscar e exibir as informações da escala
+function buscarEscala() {
+    const musicoSelecionado = document.getElementById('musico').value;
+    const diaSelecionado = document.getElementById('dia').value;
+    if (diaSelecionado === 'todos') {
+        exibirTodasAsEscalas(musicoSelecionado);
+    } else {
+        exibirInformacoes(musicoSelecionado, diaSelecionado);
+    }
+}
+
+// Função para exibir as informações de todas as escalas para o músico selecionado
+function exibirTodasAsEscalas(musico) {
+    const infoMusico = document.getElementById('infoMusico');
+    infoMusico.innerHTML = '';
+
+    // Percorre todos os dias da semana
+    const diasSemana = ["sabados", "domingos", "quintas","todos"]; // Adicione outros dias aqui, se necessário
+    for (const dia of diasSemana) {
+        const escalasDia = dados[dia];
+        for (const escalaDia of escalasDia) {
+            const musicosEscala = Object.values(escalaDia).filter(item => typeof item === 'string');
+            if (musicosEscala.includes(musico) || musicosEscala.some(item => item.includes(`(${musico})`) || item.includes(musico))) {
+                const divEscala = document.createElement('div');
+                divEscala.classList.add('escala-item');
+                divEscala.innerHTML = `<h2>${escalaDia.Data}</h2>`;
+
+                for (const [instrumento, musicoEscalado] of Object.entries(escalaDia)) {
+                    const isSelecionado = musicoEscalado.includes(musico) || musicoEscalado.includes(`(${musico})`);
+                    const isRepertorio = musicoEscalado.includes('(Repertório)');
+                    const isMicAzul = musicoEscalado.includes('(Mic Azul)');
+
+                    let classeSelecionado = '';
+                    if (isSelecionado) {
+                        classeSelecionado = 'musico-selecionado';
+                    } else if (isRepertorio) {
+                        classeSelecionado = 'musico-repertorio';
+                    } else if (isMicAzul) {
+                        classeSelecionado = 'musico-mic-azul';
+                    }
+
+                    divEscala.innerHTML += `<p class="${classeSelecionado}">${instrumento}: ${musicoEscalado}</p>`;
+                }
+
+                infoMusico.appendChild(divEscala);
+            }
+        }
+    }
+}
+
